ca620174f270bc01452956b071435bdf8bd6b9fc
naufaladrna08 <naufaladrn90@gmail.com>
2020-05-29 07:30:31 +0700

    added in and out word inline assembly

--- kernel/io.c
+++ kernel/io.c
@@ -5,6 +5,11 @@ u8 inb(u16 _port) {
 	__asm__("in al, dx");
 }
 
+u16 inw(u16 _port) {
+	__asm__("mov dx, %0" : : "g" (_port));
+	__asm__("inw ax, dx");
+}
+
 /**
  * Assembly instruction out in C
  * out port, data 
@@ -13,4 +18,10 @@ void outb(u16 _port, u8 _data) {
 	__asm__("mov dx, %0" : : "g" (_port));
 	__asm__("mov al, %0" : : "g" (_data));
 	__asm__("out dx, al");
+}
+
+void outw(u16 _port, u16 _data) {
+	__asm__("mov dx, %0" : : "g" (_port));
+	__asm__("mov ax, %0" : : "g" (_data));
+	__asm__("outw dx, ax");
 }
